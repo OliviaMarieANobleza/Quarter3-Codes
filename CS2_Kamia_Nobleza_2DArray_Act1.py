2b44ac823de7f4fff14e218e11304015ff2cfe8c
Olivia Nobleza <oliviamarie.nobleza@smc.pshs.edu.ph>
2026-01-14 22:01:49 +0800

    Added file: FA2
    disregard other uploaded FA2

---
+++ CS2_Kamia_Nobleza_2DArray_Act1.py
@@ -0,0 +1,12 @@
+prices = [
+    [52, 85, 190],
+    [50, 88, 185],
+    [55, 82, 195],
+    [53, 90, 200]
+]
+
+print("Price of milk in grocery store 2:", prices[1][1])
+print("Prices in grocery store 4:", prices[3])
+print("Updating egg price in grocery store 4 to 198.")
+prices[3][2] = 198
+print("Updated price", prices[3])
